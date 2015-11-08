c6837f0c0a875f4fb3adfb058a94158f85f8132d
Ryan Rowe <rowe.ryan.p@gmail.com>
2015-11-08 13:45:02 -0600

    REPL works

--- seriously.py
+++ seriously.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
-import sys, math, cmath, itertools, functional
+import sys, math, cmath, itertools, functools
+import commands
 
 class SeriousFunction(object):
     def __init__(self, code, srs):
@@ -14,9 +15,11 @@ class SeriousFunction(object):
 class Seriously(object):
     def __init__(self):
         self.stack = []
-        self.fn_table = {127:exit}
+        self.fn_table = commands.fn_table
     def push(self,val):
         self.stack=[val]+self.stack
+    def pop(self):
+        return self.stack.pop(0)
     def eval(self, code):
         i=0
         while i < len(code):
@@ -37,7 +40,7 @@ class Seriously(object):
                 while i<len(code) and code[i]!=':':
                     v+=code[i]
                     i+=1
-                self.push(int(s))
+                self.push(int(v))
             elif c == 'W':
                 inner = ''
                 i+=1
@@ -52,7 +55,7 @@ class Seriously(object):
                 while i<len(code) and code[i]!=']':
                     l+=code[i]
                     i+=1
-                self.push(l)
+                self.push(list(eval(l)))
             elif c == '`':
                 f = ''
                 i+=1
@@ -61,11 +64,16 @@ class Seriously(object):
                     i+=1
                 self.push(SeriousFunction(f))
             else:
-                self.fn_table.get(ord(c), lambda:pass)()
+                self.fn_table.get(ord(c), lambda x:x)(self)
             i+=1
 
 if __name__ == '__main__':
     srs = Seriously()
     while 1:
-        srs.eval(raw_input('>>> '))
-        print srs.stack
+        try:
+            srs.eval(raw_input('>>> '))
+        except:
+            exit()
+        finally:
+            print
+            print srs.stack
