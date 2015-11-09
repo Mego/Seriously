2a8648a8e1b2a51813b5c3fefe07ae7fa6b754a2
Ryan Rowe <rowe.ryan.p@gmail.com>
2015-11-08 20:19:23 -0600

    all non-functional, non-complex commands implemented

--- commands.py
+++ commands.py
@@ -1,3 +1,261 @@
 #!/usr/bin/env python
 
-fn_table={127:lambda x:exit()}
+import math, cmath, operator
+from types import *
+from __future__ import print_function, division
+
+phi = (1+5**.5)/2
+Fib = lambda n:int(phi**n/5**.5+.5)
+
+primes = [2,3]
+
+def is_prime(x):
+    global primes
+    if x in primes:
+        return True
+    if x<2:
+        return False
+    for p in filter(lambda p:p*p<=x,primes):
+        if x%p==0:
+            return False
+    n = max(primes)+2
+    while n*n<=x:
+        if x%n==0:
+            return False
+    return True
+
+def init_primes_up_to(n):
+    global primes
+    if max(primes) > n:
+        return
+    x = max(primes)+2
+    while x < n:
+        if is_prime(x):
+            primes.append(x)
+        x+=2
+
+def nth_prime(n):
+    global primes
+    while len(primes)<=n:
+        init_primes_up_to(max(primes)+100)
+    return primes[n]
+        
+def Fib_index(n):
+    a,b=5*n**2+4,5*n**2-4
+    if int(a**.5)==a:
+        return int(math.log((n*5**.5+a**.5)/2)/math.log(phi))
+    elif int(b**.5)==b:
+        return int(math.log((n*5**.5+b**.5)/2)/math.log(phi))
+    else:
+        return -1
+
+def div_fn(srs):
+    a=srs.pop()
+    if type(a) is ListType:
+        srs.push(a[-1:]+a[:-1])
+    elif type(a) in [IntType, LongType]:
+        b=srs.pop()
+        srs.push(a/b)
+    else:
+        srs.push(a)
+        
+def idiv_fn(srs):
+    a=srs.pop()
+    if type(a) is ListType:
+        srs.push(a[1:]+a[:1])
+    elif type(a) in [IntType,LongType]:
+        b=srs.pop()
+        srs.push(a//b)
+    else:
+        srs.push(a)
+
+def dupe_fn(srs):
+    a=srs.pop()
+    srs.push(a)
+    srs.push(a)
+    
+def rot2_fn(srs):
+    a,b=srs.pop(),srs.pop()
+    srs.push(a)
+    srs.push(b)
+    
+def deq_fn(srs):
+    a=srs.pop()
+    if type(a) is ListType:
+        b=a.pop(-1)
+        srs.push(a)
+        srs.push(b)
+    else:
+        srs.push(a)
+        
+def i_fn(srs):
+    a=srs.pop()
+    if type(a) is StringType and (all([c.isdigit() or c=='.' for c in a]) and a.count('.')<2):
+        srs.push(float(a))
+    elif type(a) is ListType:
+        for x in a[::-1]:
+            srs.push(x)
+    else:
+        srs.push(a)
+        
+def to_list_fn(srs):
+    srs.stack = [srs.stack]
+    
+def psh_fn(srs):
+    a=srs.pop()
+    b=srs.pop()
+    a=[b]+a
+    srs.push(a)
+    
+def p_fn(srs):
+    a=srs.pop()
+    if type(a) in [IntType, LongType]:
+        srs.push(is_prime(a))
+    elif type(a) is ListType:
+        b=a.pop()
+        srs.push(a)
+        srs.push(b)
+    else:
+        srs.push(a)
+        
+def enq_fn(srs):
+    a,b=srs.pop(),srs.pop()
+    a.append(b)
+    srs.push(a)
+    
+def flatten(lst):
+    return sum(([x] if not isinstance(x, list) else flatten(x) for x in lst), [])
+    
+def flat_explode_fn(srs):
+    tmp = []
+    while len(srs.stack)>0:
+        a = srs.pop()
+        if type(a) is StringType:
+            a = a.split('')
+        elif type(a) is ListType:
+            a = flatten(a)
+        tmp.append(a)
+    srs.stack = tmp[:]
+        
+def nrrot_fn(srs):
+    a=x.pop()
+    srs.stack=srs.stack[a:]+srs.stack[:a]
+    
+def nlrot_fn(srs):
+    a=x.pop()
+    srs.stack=srs.stack[:a]+srs.stack[a:]
+    
+def ins_top_fn(srs):
+    a=srs.pop()
+    b=srs.pop()
+    srs.stack=srs.stack[:a]+[b]+srs.stack[a:]
+    
+def ins_bot_fn(srs):
+    a=srs.pop()
+    b=srs.pop()
+    srs.stack=srs.stack[:-a]+[b]+srs.stack[-a:]
+    
+def dupe_all_fn(srs):
+    srs.stack=srs.stack[:]+srs.stack[:]
+    
+def dupe_each_fn(srs):
+    tmp=[]
+    while len(srs.stack)>0:
+        a=srs.pop()
+        tmp.append(a)
+        tmp.append(a)
+    srs.stack=tmp[:]
+        
+fn_table={32:lambda x:x.push(len(x.stack)),
+          33:lambda x:x.push(math.factorial(x.pop())),
+          37:lambda x:x.push(x.pop()%x.pop()),
+          38:lambda x:x.push(x.pop() & x.pop()),
+          40:lambda x:x.push(x.stack.pop(-1))
+          41:lambda x:x.append(x.pop())
+          42:lambda x:x.push(x.pop()*x.pop()),
+          43:lambda x:x.push(x.pop()+x.pop()),
+          44:lambda x:x.push(input()),
+          45:lambda x:x.push(x.pop()-x.pop()),
+          46:lambda x:print(x.pop())
+          47:div_fn,
+          59:dupe_fn,
+          60:lambda x:x.push(int(x.pop()<x.pop())),
+          61:lambda x:x.push(int(x.pop()==x.pop())),
+          62:lambda x:x.push(int(x.pop()>x.pop())),
+          63:lambda x:x,
+          64:rot2_fn,
+          65:lambda x:x.push(abs(x.pop())),
+          67:lambda x:x.push(math.cos(x.pop())),
+          69:lambda x:x.push(math.erf(x.pop())),
+          70:lambda x:x.push(Fib(x.pop())),
+          75:lambda x:x.push(ceil(x.pop())),
+          76:lambda x:x.push(floor(x.pop())),
+          80:lambda x:x.push(nth_prime(x.pop())),
+          83:lambda x:x.push(math.sin(x.pop())),
+          84:lambda x:x.push(math.tan(x.pop())),
+          85:lambda x:x.push(list(set(x.pop()).union(x.pop()))),
+          90:lambda x:x.push(zip(x.pop(),x.pop())),
+          92:idiv_fn,
+          94:lambda x:x.push(math.pow(x.pop(),x.pop())),
+          95:lambda x:x.push(math.log(x.pop())),
+          98:lambda x:x.push(int(bool(x))),
+          99:lambda x:x.push(chr(x.pop()%256)),
+          100:deq_fn,
+          101:lambda x:x.push(math.exp(x.pop())),
+          102:lambda x:x.push(Fib_index(x.pop())),
+          103:lambda x:x.push(math.gcd(x.pop(),x.pop())),
+          104:lambda x:x.push(math.hypot(x.pop(),x.pop())),
+          105:i_fn,
+          106:lambda x:x.push(str.join(x.pop(),x.pop())),
+          107:to_list_fn,
+          108:lambda x:x.push(len(x.pop())),
+          109:lambda x:map(x.push,math.modf(x.pop())),
+          110:lambda x:map(x.push,(lambda y:[y for _ in range(y)])(x.pop())),
+          111:psh_fn,
+          112:p_fn,
+          113:enq_fn,
+          114:lambda x:map(x.push,x.pop().split('')[::-1]),
+          115:lambda x:x.push(math.sgn(x.pop())),
+          116:flat_explode_fn,
+          122:lambda x:map(x.eval,(lambda y:['.' for _ in range(y)])(x.pop())),
+          123:nrrot_fn,
+          124:lambda x:x.push(x.pop() | x.pop()),
+          125:nlrot_fn,
+          126:lambda x:x.push(~x.pop()),
+          127:lambda x:exit(),
+          129:lambda x:map(print,[x.pop() for _ in len(x.stack)]),
+          131:lambda x:x.push(math.asin(x.pop())),
+          132:lambda x:x.push(math.acos(x.pop())),
+          133:lambda x:x.push(math.atan(x.pop())),
+          134:lambda x:x.push(math.atan2(x.pop(),x.pop())),
+          135:lambda x:x.push(math.asinh(x.pop())),
+          136:lambda x:x.push(math.acosh(x.pop())),
+          137:lambda x:x.push(math.atanh(x.pop())),
+          142:lambda x:x.push(math.sinh(x.pop())),
+          143:lambda x:x.push(math.cosh(x.pop())),
+          144:lambda x:x.push(math.tanh(x.pop())),
+          155:lambda x:x.push(math.copysign(x.pop(),x.pop())),
+          166:lambda x:x.push(x.pop()**2),
+          167:lambda x:x.push(math.degrees(x.pop())),
+          169:lambda x:x.push(x.pop()+2),
+          170:lambda x:x.push(x.pop()-2),
+          171:lambda x:x.push(x.pop()/2),
+          172:lambda x:x.push(x.pop()/4),
+          174:ins_bot_fn,
+          175:ins_top_fn,
+          179:dupe_all_fn,
+          197:dupe_each_fn,
+          203:lambda x:x.push(math.pi),
+          204:lambda x:x.push(math.e),
+          226:lambda x:x.push(math.gamma(x.pop())),
+          227:lambda x:x.push(reduce(operator.mul,x.pop(),1)),
+          228:lambda x:x.push(sum(x.pop())),
+          237:lambda x:x.push(phi),
+          239:lambda x:x.push(set(x.pop()).intersection(x.pop())),
+          241:lambda x:x.push(-x.pop()),
+          242:lambda x:x.push(x.pop()>=x.pop()),
+          243:lambda x:x.push(x.pop()<=x.pop()),
+          247:lambda x:x.push(int(x.pop())),
+          248:lambda x:x.push(math.radians(x.pop())),
+          251:lambda x:x.push(x.pop()**.5),
+          }
--- seriously.py
+++ seriously.py
@@ -3,14 +3,28 @@
 import sys, math, cmath, itertools, functools
 import commands
 
+def NinetyNineBottles():
+    x = 99
+    for i in range(99):
+        w = 'Take one down and pass it around, '+str((x-(i+1)))+' bottle of beer on the wall.'
+        y = str((x-i))+' bottles of beer on the wall, '+str((x-i))+' bottles of beer'
+        z = 'Go to the Store and buy some more, '+str(x)+' bottle of beer on the wall.'
+        if i == (x-1):
+            print(y + '\n' + z)
+        else:
+            print(y + '\n' + w)
+        i += 1
+
 class SeriousFunction(object):
     def __init__(self, code, srs):
         self.srs = srs
         self.code = code
     def __call__(self):
         self.srs.eval(self.code)
-    def __repr__(self):
+    def __str__(self):
         return '`%s`'%code
+    def __repr__(self):
+        self.__call__()
 
 class Seriously(object):
     def __init__(self):
@@ -20,6 +34,8 @@ class Seriously(object):
         self.stack=[val]+self.stack
     def pop(self):
         return self.stack.pop(0)
+    def append(self, val):
+        self.stack+=[val]
     def eval(self, code):
         i=0
         while i < len(code):
@@ -63,8 +79,22 @@ class Seriously(object):
                     f+=code[i]
                     i+=1
                 self.push(SeriousFunction(f))
+            elif c == 'Q' and len(self.stack) == 0:
+                print code
+            elif ord(c) in range(48,58):
+                self.push(int(c))
+            elif c == 'H' and len(self.stack) == 0:
+                print 'Hello, World!'
+            elif c == 'N' and len(self.stack) == 0:
+                print NinetyNineBottles()
+            elif ord(c) == 130:
+                self.stack = []
             else:
-                self.fn_table.get(ord(c), lambda x:x)(self)
+                old_stack = self.stack[:]
+                try:
+                    self.fn_table.get(ord(c), lambda x:x)(self)
+                except:
+                    self.stack = old_stack[:]
             i+=1
 
 if __name__ == '__main__':
@@ -72,7 +102,7 @@ if __name__ == '__main__':
     while 1:
         try:
             srs.eval(raw_input('>>> '))
-        except:
+        except EOFError:
             exit()
         finally:
             print
