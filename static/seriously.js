f75b598702a132dcbaaa0d9c8fbcfa95fa01d78b
Ryan Rowe <Mego@users.noreply.github.com>
2015-11-11 13:25:44 -0600

    fix cp437 char generation

--- static/seriously.js
+++ static/seriously.js
@@ -1,6 +1,7 @@
-function genUni() {
-	var code = prompt("Generate Unicode Character:");
-	$('#code').val($('#code').val() + String.fromCharCode(parseInt(code)));
+function genChar() {
+    var cp437 = (function(){ var d = "\u0000\u0001\u0002\u0003\u0004\u0005\u0006\u0007\b\t\n\u000b\f\r\u000e\u000f\u0010\u0011\u0012\u0013\u0014\u0015\u0016\u0017\u0018\u0019\u001a\u001b\u001c\u001d\u001e\u001f !\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~ÇüéâäàåçêëèïîìÄÅÉæÆôöòûùÿÖÜ¢£¥₧ƒáíóúñÑªº¿⌐¬½¼¡«»░▒▓│┤╡╢╖╕╣║╗╝╜╛┐└┴┬├─┼╞╟╚╔╩╦╠═╬╧╨╤╥╙╘╒╓╫╪┘┌█▄▌▐▀αßΓπΣσµτΦΘΩδ∞φε∩≡±≥≤⌠⌡÷≈°∙·√ⁿ²■ ", D = [], e = {}; for(var i=0;i!=d.length;++i) { if(d.charCodeAt(i) !== 0xFFFD) e[d[i]] = i; D[i] = d.charAt(i); } return {"enc": e, "dec": D }; })();
+	var code = prompt("Generate CP437 Character:");
+	$('#code').val($('#code').val() + cp437.dec[parseInt(code)]);
 	updateByteCount();
 };
 
