b62393127efff46ae34b7ac0fe2cbd28168a2e5c
Ryan Rowe <Mego@users.noreply.github.com>
2015-11-11 18:17:15 -0600

    unbreak explanations

--- ide.py
+++ ide.py
@@ -2,7 +2,6 @@
 from flask import Flask, render_template, url_for, request
 from subprocess import Popen, PIPE, check_call
 import os, string
-from make_explanations import make_explanations
 
 app = Flask(__name__)
 
@@ -24,6 +23,25 @@ def index():
     else:
         return render_template('primary.html')
 
+def make_explanations():
+    lines = []
+    with open('commands.txt','rb') as f:
+        for line in f:
+            lines.append(line)
+    i=0
+    while not lines[i][0].isdigit():
+        i+=1
+    ex = "var explanations = {\n%s\n};"
+    exps = []
+    for line in lines[i:]:
+        toks = line.split(':')
+        val,desc = toks[0],':'.join(toks[1:])
+        val = int(val[:val.index(' ')])
+        desc = repr(desc.strip())
+        exps.append('%s:%s'%(val,desc))
+    with open('static/explanations.js','wb') as f:
+        f.write(ex%(',\n'.join(exps)))
+        
 @app.route('/link/')
 @app.route('/link/<link>')
 def link(link='code=%22Error+in+linking+code%22o&input='):
