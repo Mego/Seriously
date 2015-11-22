95a80f6b3af8271fd892d0025c622f62d7264823
Ryan Rowe <Mego@users.noreply.github.com>
2015-11-21 21:21:42 -0600

    translate.py works with hex values now

--- translate.py
+++ translate.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python
 
-"""Translates a string of whitespace-separated integers to CP437 characters"""
+"""Translates a string of hex values to CP437 characters. Characters not in [0-9A-F] are ignored"""
 
-import sys
+import sys, re
 s = sys.stdin.read()
-print ''.join(map(lambda x:chr(int(x)),s.split()))
+s=re.sub(r"[^0-9A-F]","",s)
+r=''
+i=0
+while i < len(s):
+    r = chr(int(s[i:i+2],16))
+    i+=2
+print r
