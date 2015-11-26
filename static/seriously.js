85e172e8fe26848354ed4e2d53744287b9e45f1e
Ryan Rowe <Mego@users.noreply.github.com>
2015-11-26 02:01:49 -0600

    use hex values in commands.txt

--- static/seriously.js
+++ static/seriously.js
@@ -40,6 +40,10 @@ if (!String.prototype.repeat) {
   }
 }
 
+function toHex(d) {
+    return  ("0"+(Number(d).toString(16))).slice(-2).toUpperCase()
+}
+
 var cp437 = {};
 
 cp437.codepage = "\u0000\u0001\u0002\u0003\u0004\u0005\u0006\u0007\b\t\n\u000b\f\r\u000e\u000f\u0010\u0011\u0012\u0013\u0014\u0015\u0016\u0017\u0018\u0019\u001a\u001b\u001c\u001d\u001e\u001f !\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~ÇüéâäàåçêëèïîìÄÅÉæÆôöòûùÿÖÜ¢£¥₧ƒáíóúñÑªº¿⌐¬½¼¡«»░▒▓│┤╡╢╖╕╣║╗╝╜╛┐└┴┬├─┼╞╟╚╔╩╦╠═╬╧╨╤╥╙╘╒╓╫╪┘┌█▄▌▐▀αßΓπΣσµτΦΘΩδ∞φε∩≡±≥≤⌠⌡÷≈°∙·√ⁿ²■ "
@@ -134,7 +138,7 @@ function getExplanation(code, indent) {
             explain = 'push the string "' + code.charAt(x) +'"'
         }
         else if(cp437.decode(c) > -1)
-            explain += ind + explanations[cp437.decode(c)] +'\r\n';
+            explain += ind + explanations[toHex(cp437.decode(c))] +'\r\n';
     }
     if(string) {
         var prev = code.lastIndexOf('"',x-1);
