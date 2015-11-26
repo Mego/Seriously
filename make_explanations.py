ee2e2107cda9db3c665aee0fe01133f3156c6c90
Ryan Rowe <Mego@users.noreply.github.com>
2015-11-26 02:01:49 -0600

    use hex values in commands.txt

--- make_explanations.py
+++ make_explanations.py
@@ -13,9 +13,9 @@ def make_explanations():
     for line in lines[i:]:
         toks = line.split(':')
         val,desc = toks[0],':'.join(toks[1:])
-        val = int(val[:val.index(' ')])
+        val = val[:val.index(' ')]
         desc = repr(desc.strip())
-        exps.append('%s:%s'%(val,desc))
+        exps.append('"%s":%s'%(val,desc))
     with open('static/explanations.js','wb') as f:
         f.write(ex%(',\n'.join(exps)))
         
