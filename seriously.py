8488a776a72b0d73f6fee534e354c118108b3f24
Ryan Rowe <Mego@users.noreply.github.com>
2016-03-02 00:02:28 -0600

    full Python 3 support

--- seriously.py
+++ seriously.py
@@ -1,13 +1,30 @@
-#!/usr/bin/env python3
-import traceback, argparse, readline, hashlib, binascii, random
+#!/usr/bin/python3
+# -*- encoding: utf-8 -*-
+import argparse
+import ast
+import binascii
+import hashlib
+import random
+import readline
+import sys
+import traceback
 from types import *
-from cp437 import CP437
-import commands
+import SeriouslyCommands
+
+anytype = SeriouslyCommands.anytype
+
+cp437table = ''.join(map(chr,list(range(128)))) + "ÇüéâäàåçêëèïîìÄÅÉæÆôöòûùÿÖÜ¢£¥₧ƒáíóúñÑªº¿⌐¬½¼¡«»░▒▓│┤╡╢╖╕╣║╗╝╜╛┐└┴┬├─┼╞╟╚╔╩╦╠═╬╧╨╤╥╙╘╒╓╫╪┘┌█▄▌▐▀αßΓπΣσµτΦΘΩδ∞φε∩≡±≥≤⌠⌡÷≈°∙·√ⁿ²■ "
+
+def ord_cp437(c):
+    return cp437table.index(c)
+    
+def chr_cp437(o):
+    return cp437table[o]
 
 class Seriously(object):
     @classmethod
     def _make_new(cls,init_stack=[], debug_mode=False, repl_mode=False):
-        return cls(init_stack,debug_mode)
+        return cls(init_stack, debug_mode, repl_mode)
     def make_new(self,*stack):
         return self._make_new(init_stack=list(stack), debug_mode=self.debug_mode)
         return res
@@ -15,35 +32,30 @@ class Seriously(object):
         self.stack = init_stack
         self.debug_mode=debug_mode
         self.repl_mode = repl_mode
-        self.fn_table = commands.fn_table
+        self.fn_table = SeriouslyCommands.fn_table
         self.code = ''
         self.hex_mode = hex_mode
         self.preserve = False
         self.pop_counter = 0
     def push(self,val):
-        self.stack=[val]+self.stack
+        self.stack.append(val)
     def pop(self):
-        return self.stack.pop(0) if not self.preserve else self.preserve_pop()
+        return self.stack.pop() if not self.preserve else self.preserve_pop()
     def preserve_pop(self):
-        v = self.stack[self.pop_counter] if self.stack and len(self.stack) > self.pop_counter else None
-        self.pop_counter += 1
+        v = self.stack.pop()
+        self.push(v)
         return v
     def peek(self):
-        return self.stack[0] if self.stack else None
-    def append(self, val):
-        self.stack+=[val]
+        return self.stack[-1] if self.stack else None
+    def prepend(self, val):
+        self.stack[:] = [val] + self.stack
     def toggle_preserve(self):
         self.preserve = not self.preserve
     def eval(self, code, print_at_end=True):
         if self.hex_mode:
-            code = binascii.unhexlify(code)
-        key = binascii.unhexlify('1f1733f7cc54447e9f5568e50af437ddea0039600d345af3d708f1a4dc4a40260bd39ed1')
-        if hashlib.sha256(code[:10]).hexdigest() == 'd0cedf8c945e712024b7dfd69bf504ffb3fec1232b294c5602507dbe439a57fb':
-            rnd = random.Random()
-            rnd.seed(int(binascii.hexlify(code[:10]),16))
-            lock = ''.join([chr(rnd.randrange(256)) for i in range(len(key))])
-            exec ''.join(map(lambda x,y:chr(ord(x)^ord(y)),lock,key)) in globals(),locals()
-            return
+            code = binascii.unhexlify(code).decode('cp437')
+        if self.debug_mode:
+            print(code)
         i=0
         if self.repl_mode:
             self.code += code
@@ -83,7 +95,7 @@ class Seriously(object):
                         val = eval(v)
                     except:
                         pass
-                    val = val if type(val) in [IntType,LongType,FloatType,ComplexType] else 0
+                    val = val if anytype(val, int, float, complex) else 0
                     self.push(val)
                 elif c == 'W':
                     inner = ''
@@ -92,7 +104,7 @@ class Seriously(object):
                         inner+=code[i]
                         i+=1
                     if self.debug_mode:
-                        print "while loop code: %s"%inner
+                        print("while loop code: %s"%inner)
                     while self.peek():
                         self.eval(inner, print_at_end=False)
                 elif c == '[':
@@ -108,15 +120,15 @@ class Seriously(object):
                     while i<len(code) and code[i]!='`':
                         f+=code[i]
                         i+=1
-                    self.push(commands.SeriousFunction(f))
+                    self.push(SeriouslyCommands.SeriousFunction(f))
                 elif ord(c) in range(48,58):
                     self.push(int(c))
                 else:
                     if self.debug_mode:
-                        print binascii.hexlify(chr(ord_cp437(c))).upper()
+                        print(binascii.hexlify(chr(ord_cp437(c)).encode()).decode().upper())
                     self.fn_table.get(ord_cp437(c), lambda x:x)(self)
                     if self.debug_mode:
-                        print self.stack
+                        print(self.stack)
             except SystemExit:
                 exit()
             except KeyboardInterrupt:
@@ -129,20 +141,20 @@ class Seriously(object):
                 i+=1
         if not self.repl_mode and print_at_end:
             while len(self.stack) > 0:
-                print self.pop()
+                print(self.pop())
 
 def srs_repl(debug_mode=False, quiet_mode=False, hex=False):
     srs = Seriously(repl_mode=True, debug_mode=debug_mode, hex_mode=hex)
     while 1:
         try:
-            srs.eval(raw_input('' if quiet_mode else '>>> '))
+            srs.eval(input('' if quiet_mode else '>>> '))
         except EOFError:
             exit()
         finally:
             if not quiet_mode:
-                print '\n'
-                print srs.stack
-
+                print('\n')
+                print(srs.stack)
+            
 def srs_exec(debug_mode=False, file_obj=None, code=None, hex=False):
     srs = Seriously(debug_mode=debug_mode, hex_mode=hex)
     if file_obj:
@@ -150,7 +162,7 @@ def srs_exec(debug_mode=False, file_obj=None, code=None, hex=False):
         file_obj.close()
     else:
         srs.eval(code)
-
+                
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Run the Seriously interpreter")
     parser.add_argument("-d", "--debug", help="turn on debug mode", action="store_true")
@@ -159,10 +171,10 @@ if __name__ == '__main__':
     parser.add_argument("-i", "--ide", help="turn on IDE mode, which disables unsafe commands", action="store_true")
     group = parser.add_mutually_exclusive_group()
     group.add_argument("-c", "--code", help="run the specified code")
-    group.add_argument("-f", "--file", help="specify an input file", type=argparse.FileType('rb'))
+    group.add_argument("-f", "--file", help="specify an input file", type=argparse.FileType('r'))
     args = parser.parse_args()
     if args.ide:
-        commands.fn_table[0xF0]=lambda x:x
+        commands.fn_table[0xF0] = lambda x: x.push(ast.literal_eval(x.pop()))
     if args.code or args.file:
         srs_exec(args.debug, args.file, args.code, args.hex)
     else:
