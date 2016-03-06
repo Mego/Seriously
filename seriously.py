7ed36796763a79fa584f4bf029e8945a33903c47
Ryan Rowe <Mego@users.noreply.github.com>
2016-03-06 00:33:57 -0600

    unit tests

--- seriously.py
+++ seriously.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 import argparse
-import ast
+from ast import literal_eval
 import binascii
 import hashlib
 import random
@@ -17,35 +17,48 @@ ord_cp437 = CP437.ord
 
 chr_cp437 = CP437.chr
 
+
 class Seriously(object):
     @classmethod
-    def _make_new(cls,init_stack=[], debug_mode=False):
-        return cls(init_stack, debug_mode)
-    def make_new(self,*stack):
-        return self._make_new(init_stack=list(stack), debug_mode=self.debug_mode)
+    def _make_new(cls, init=None, debug_mode=False):
+        return cls([] if init is None else init, debug_mode)
+
+    def make_new(self, *stack):
+        return self._make_new(init=list(stack), debug_mode=self.debug_mode)
         return res
-    def __init__(self, init_stack=[], debug_mode=False, hex_mode=False):
-        self.stack = init_stack
-        self.debug_mode=debug_mode
+
+    def __init__(self, init_stack=None, debug_mode=False, hex_mode=False):
+        self.stack = init_stack if init_stack is not None else []
+        self.debug_mode = debug_mode
         self.fn_table = SeriouslyCommands.fn_table
         self.code = ''
         self.hex_mode = hex_mode
         self.preserve = False
         self.pop_counter = 0
-    def push(self,val):
+
+    def push(self, val):
         self.stack.append(val)
+
     def pop(self):
         return self.stack.pop() if not self.preserve else self.preserve_pop()
+
     def preserve_pop(self):
         v = self.stack.pop()
         self.push(v)
         return v
+
     def peek(self):
         return self.stack[-1] if self.stack else None
+
     def prepend(self, val):
         self.stack[:] = [val] + self.stack
+
     def toggle_preserve(self):
         self.preserve = not self.preserve
+
+    def clear_stack(self):
+        self.stack = list()
+
     def eval(self, code):
         if self.hex_mode:
             tmp = ''
@@ -54,7 +67,7 @@ class Seriously(object):
             code = tmp
         if self.debug_mode:
             print(code)
-        i=0
+        i = 0
         self.code = code
         while i < len(code):
             old_stack = self.stack[:]
@@ -62,58 +75,64 @@ class Seriously(object):
                 c = code[i]
                 if c == '"':
                     s = ""
-                    i+=1
-                    while i<len(code) and code[i]!='"':
-                        s+=code[i]
-                        i+=1
+                    i += 1
+                    while i < len(code) and code[i] != '"':
+                        s += code[i]
+                        i += 1
                     self.push(s)
                 elif c == "'":
-                    i+=1
+                    i += 1
                     self.push(code[i])
                 elif c == ':':
                     v = ""
-                    i+=1
-                    while i<len(code) and code[i] in '0123456789.ij+-':
-                        v+=code[i]
-                        i+=1
-                    i-=1
+                    i += 1
+                    while i < len(code) and code[i] in '0123456789.ij+-':
+                        v += code[i]
+                        i += 1
+                    i -= 1
                     val = 0
+                    v = v.replace('i', 'j')
+                    if self.debug_mode:
+                        print(v)
                     try:
-                        val = eval(v)
+                        val = literal_eval(v)
+                        if self.debug_mode:
+                            print(val)
                     except:
-                        pass
+                        if self.debug_mode:
+                            print("Failed to eval numeric")
                     val = val if anytype(val, int, float, complex) else 0
                     self.push(val)
                 elif c == 'W':
                     inner = ''
-                    i+=1
-                    while i<len(code) and code[i]!='W':
-                        inner+=code[i]
-                        i+=1
+                    i += 1
+                    while i < len(code) and code[i] != 'W':
+                        inner += code[i]
+                        i += 1
                     if self.debug_mode:
-                        print("while loop code: %s"%inner)
+                        print("while loop code: {}".format(inner))
                     while self.peek():
-                        self.eval(inner, print_at_end=False)
+                        self.eval(inner)
                 elif c == '[':
                     l = ''
-                    i+=1
-                    while i<len(code) and code[i]!=']':
-                        l+=code[i]
-                        i+=1
-                    self.push(eval('[%s]'%l))
+                    i += 1
+                    while i < len(code) and code[i] != ']':
+                        l += code[i]
+                        i += 1
+                    self.push(literal_eval('[{}]'.format(l)))
                 elif c == '`':
                     f = ''
-                    i+=1
-                    while i<len(code) and code[i]!='`':
-                        f+=code[i]
-                        i+=1
+                    i += 1
+                    while i < len(code) and code[i] != '`':
+                        f += code[i]
+                        i += 1
                     self.push(SeriouslyCommands.SeriousFunction(f))
-                elif ord(c) in range(48,58):
+                elif ord(c) in range(48, 58):
                     self.push(int(c))
                 else:
                     if self.debug_mode:
-                        print(binascii.hexlify(chr(ord_cp437(c)).encode()).decode().upper())
-                    self.fn_table.get(ord_cp437(c), lambda x:x)(self)
+                        print("{:2X}".format(ord_cp437(c)))
+                    self.fn_table.get(ord_cp437(c), lambda x: x)(self)
                     if self.debug_mode:
                         print(self.stack)
             except SystemExit:
@@ -125,8 +144,8 @@ class Seriously(object):
                     traceback.print_exc()
                 self.stack = old_stack[:]
             finally:
-                i+=1
-        return self.stack
+                i += 1
+        return self.stack[::-1]
 
 
 def srs_exec(debug_mode=False, file_obj=None, code=None, hex=False):
@@ -139,16 +158,25 @@ def srs_exec(debug_mode=False, file_obj=None, code=None, hex=False):
         for x in srs.eval(code):
             print(x)
 
+
+def ide_mode():
+    SeriouslyCommands.fn_table[0xF0] = lambda x: x.push(literal_eval(x.pop()))
+
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description="Run the Seriously interpreter")
-    parser.add_argument("-d", "--debug", help="turn on debug mode", action="store_true")
-    parser.add_argument("-q", "--quiet", help="turn off REPL prompts and automatic stack printing, only print code STDOUT output", action="store_true")
-    parser.add_argument("-x", "--hex", help="turn on hex mode (code is taken in hex values instead of binary bytes)", action="store_true")
-    parser.add_argument("-i", "--ide", help="turn on IDE mode, which disables unsafe commands", action="store_true")
+    parser = argparse.ArgumentParser(
+                description="Run the Seriously interpreter")
+    parser.add_argument("-d", "--debug", help="turn on debug mode",
+                        action="store_true")
+    parser.add_argument("-x", "--hex",
+                        help="turn on hex mode (code is taken in hex values)",
+                        action="store_true")
+    parser.add_argument("-i", "--ide",
+                        help="disable unsafe commands", action="store_true")
     group = parser.add_mutually_exclusive_group(required=True)
     group.add_argument("-c", "--code", help="run the specified code")
-    group.add_argument("-f", "--file", help="specify an input file", type=argparse.FileType('r'))
+    group.add_argument("-f", "--file", help="specify an input file",
+                       type=argparse.FileType('r'))
     args = parser.parse_args()
     if args.ide:
-        commands.fn_table[0xF0] = lambda x: x.push(ast.literal_eval(x.pop()))
+        ide_mode()
     srs_exec(args.debug, args.file, args.code, args.hex)
--- tests.py
+++ tests.py
@@ -1,126 +1,225 @@
 #!/usr/bin/env python3
-import multiprocessing
-import subprocess
+from io import StringIO
 import sys
+import unittest
 from lib.cp437 import CP437
+from seriously import Seriously
+from SeriouslyCommands import SeriousFunction
 
 ord_cp437 = CP437.ord
 chr_cp437 = CP437.chr
 
 
-class SeriousError(SystemExit):
-    def __init__(self, *args):
-        super().__init__()
-        self.args = args
-
-    def __str__(self):
-        return repr(self.args)
+def serious_check(*args):
+    pass
 
 
-def serious_call(args):
-    code, result, cinput, errors = args
-    p = subprocess.Popen(['python3', 'seriously.py', '-c', code],
-                         stdin=subprocess.PIPE, stdout=subprocess.PIPE,
-                         universal_newlines=True)
-    out, err = p.communicate(cinput)
-    return code, cinput, out, err, result, errors
+class SeriousTest(unittest.TestCase):
+    def setUp(self):
+        self.srs = Seriously()
+
+    def tearDown(self):
+        self.srs = None
+        if sys.stdin is not sys.__stdin__:
+            sys.stdin.close()
+            sys.stdin = sys.__stdin__
+
+    def setInput(self, str):
+        if sys.stdin is not sys.__stdin__:
+            sys.stdin.close()
+        sys.stdin = StringIO(str)
+
+    def assert_serious(self, code, output, input=None, close=False):
+        if input is not None:
+            self.setInput(input)
+        if not close:
+            self.assertEqual(self.srs.eval(code), output)
+        else:
+            for a, b in zip(self.srs.eval(code), output):
+                self.assertAlmostEqual(a, b)
+        self.srs.clear_stack()
 
 
-run_queue = []
+class IOTests(SeriousTest):
+    def test_raw_input(self):
+        self.assert_serious(chr_cp437(0x09), ['a'], "a\n")
+        self.assert_serious(chr_cp437(0x0C), ['abc\n'], "abc\n")
+
+    def test_formatted_input(self):
+        self.assert_serious(',', ['a'], '"a"\n')
+
+        self.assert_serious(',', [12345], '12345\n')
+
+        self.assert_serious(',', [[3, 2, 1]], '[3,2,1]\n')
+
+        self.assert_serious(',', [[3, 2, 1]], '3, 2, 1\n')
 
 
-def serious_check(code, result, cinput='', errors=True):
-    global run_queue
-    run_queue.append((code, result, cinput, errors))
+class LiteralTests(SeriousTest):
+    def test_strings(self):
+        self.assert_serious('"a', ['a'])
+
+        self.assert_serious('"a"', ['a'])
+
+        self.assert_serious("'a", ['a'])
+
+        self.assert_serious("'a'b", ['b', 'a'])
+
+    def test_digits(self):
+        for i in range(10):
+            with self.subTest(i=i):
+                self.assert_serious(str(i), [i])
+
+    def test_numerics(self):
+        self.assert_serious(':12345', [12345])
+
+        self.assert_serious(':-1', [-1])
+
+        self.assert_serious(':.5', [0.5])
+
+        self.assert_serious(':1+2.2i', [1+2.2j])
+
+    def test_functions(self):
+        self.assert_serious("`foo`", [SeriousFunction("foo")])
+
+    def test_eval(self):
+        self.assert_serious('"len(set([1,2,2,3]))"{}'.format(chr_cp437(0xF0)),
+                            [3])
 
 
-def run_tests():
-    global run_queue
-    with multiprocessing.Pool() as pool:
-        r = pool.imap_unordered(serious_call, run_queue)
-        for res in r:
-            code, cinput, output, error, result, errors = res
-            res2 = {'code': code, 'input': cinput, 'output': output,
-                    'error': error, 'result': result, 'errors': errors}
-            if (errors and error) or (output != result):
-                print("Bad: {}".format(res2))
-                return False
-    return True
+class StackTests(SeriousTest):
+    def test_count(self):
+        self.assert_serious('1 ', [1, 1])
+
+    def test_rotations(self):
+        self.assert_serious('123(', [1, 3, 2])
+        self.assert_serious('123)', [2, 1, 3])
+        self.assert_serious('123@', [2, 3, 1])
+
+    def test_quine(self):
+        self.assert_serious('Q;', ['Q;', 'Q;'])
+
+    def test_loop(self):
+        self.assert_serious('5W;D', [0, 1, 2, 3, 4, 5])
+
+    def test_misc(self):
+        self.assert_serious('123a', [1, 2, 3])
+        with self.assertRaises(SystemExit):
+            self.srs.eval('123'+chr_cp437(0x7F))
+        self.srs.clear_stack()
+        self.assert_serious('123'+chr_cp437(0xB3), [3, 2, 1, 3, 2, 1])
+        self.assert_serious('123'+chr_cp437(0xC5), [3, 3, 2, 2, 1, 1])
+        self.assert_serious('12'+chr_cp437(0xC6), [1, 1])
+
+
+class RegisterTests(SeriousTest):
+    def test_push_pop(self):
+        self.assert_serious('1{}2{}{}{}'.format(
+                            chr_cp437(0xBB), chr_cp437(0xBC),
+                            chr_cp437(0xBE), chr_cp437(0xBD)), [1, 2])
+        self.assert_serious('53{}3{}'.format(chr_cp437(0xBF),
+                                             chr_cp437(0xC0)), [5])
+
+    def test_input(self):
+        self.assert_serious(chr_cp437(0xCA)+chr_cp437(0xBD)+chr_cp437(0xBE),
+                            [2, 'b'],
+                            '"b"\n2\n')
+
+
+class MathTests(SeriousTest):
+    def test_arithmetic(self):
+        self.assert_serious('23+', [5])
+        self.assert_serious('23-', [1])
+        self.assert_serious('23*', [6])
+        self.assert_serious('24\\', [2])
+        self.assert_serious('25/', [2.5])
+        self.assert_serious('25\\', [2])
+
+    def test_lists(self):
+        self.assert_serious('[1][1,2]-', [[2]])
+        self.assert_serious('[2,3];*', [13])
+        self.assert_serious('[1,2,3]M', [3])
+        self.assert_serious('[1,2,3]m', [1])
+        self.assert_serious('[1,2,3,4]'+chr_cp437(0xE4), [10])
+        self.assert_serious('[2.5,2.5]'+chr_cp437(0xE4), [5.0])
+        self.assert_serious('[1,2,3,4]'+chr_cp437(0xE3), [24])
+        self.assert_serious('[1,2,3,4]'+chr_cp437(0xBA), [2.5])
+        self.assert_serious('[1,2,3,3]'+chr_cp437(0x9A), [3])
+
+    def test_filters(self):
+        self.assert_serious("[4]12'3k"+chr_cp437(0x8D), [[1, 2]])
+        self.assert_serious("[4]12'3k"+chr_cp437(0x92), [['3']])
+        self.assert_serious("[4]12'3k"+chr_cp437(0xA5), [[[4]]])
+
+    def test_sequences(self):
+        self.assert_serious('4p', [0])
+        self.assert_serious(':23p', [1])
+        self.assert_serious('3P', [7])
+        self.assert_serious('8f', [6])
+        self.assert_serious(':16'+chr_cp437(0xDF), ['0123456789ABCDEF'])
+        self.assert_serious('2'+chr_cp437(0xB9), [[1, 2, 1]])
+
+    def test_trig(self):
+        trig_fns = {
+            'sin': ('S', chr_cp437(0x83)),
+            'sinh': (chr_cp437(0x8E), chr_cp437(0x87)),
+            'cos': ('C', chr_cp437(0x84)),
+            'cosh': (chr_cp437(0x8F), chr_cp437(0x88)),
+            'tan': ('T', chr_cp437(0x85)),
+            'tanh': (chr_cp437(0x90), chr_cp437(0x89)),
+        }
+        for fn in trig_fns:
+            with self.subTest(function=fn):
+                fns = trig_fns[fn]
+                self.assert_serious('1{}{}'.format(*fns), [1], close=True)
+
+
+class StringAndListTests(SeriousTest):
+    def test_format(self):
+        self.assert_serious('[2,3]"{}.{}"f', ["2.3"])
+        self.assert_serious('[2,3]"%d.%d"%', ["2.3"])
+
+    def test_modify(self):
+        self.assert_serious('52[2,3,4]T', [[2, 3, 5]])
+        self.assert_serious('52"234"T', ["235"])
+
+    def test_ords(self):
+        self.assert_serious('"abc"O', [[0x61, 0x62, 0x63]])
+
+    def test_string_methods(self):
+        self.assert_serious('" ab c "'+chr_cp437(0x93), ["ab c"])
+        self.assert_serious('" ab c "'+chr_cp437(0x94), ["ab c "])
+        self.assert_serious('" ab c "'+chr_cp437(0x95), [" ab c"])
+        self.assert_serious('"CAFE babe 123"'+chr_cp437(0x96),
+                            ["CAFE BABE 123"])
+        self.assert_serious('"CAFE babe 123"'+chr_cp437(0x97),
+                            ["cafe babe 123"])
+        self.assert_serious('"CAFE babe 123"'+chr_cp437(0x98),
+                            ["Cafe Babe 123"])
+        self.assert_serious('"CAFE babe 123"'+chr_cp437(0x99),
+                            ["cafe BABE 123"])
+
+
+class BaseConversionTests(SeriousTest):
+    def test_bases(self):
+        self.assert_serious('2:5.5'+chr_cp437(0xAD), ["101.1"])
+
+
+class ListTests(SeriousTest):
+    def test_lists(self):
+        self.assert_serious('2[1,2,3]'+chr_cp437(0xCF),
+                            [[[1, 2], [1, 3], [2, 3]]])
+        self.assert_serious('2[1,2,3]'+chr_cp437(0xD0),
+                            [[[1, 2], [1, 3], [2, 1],
+                             [2, 3], [3, 1], [3, 2]]])
+        self.assert_serious('2[1,2,3]'+chr_cp437(0xF9),
+                            [[[1, 1], [1, 2], [1, 3],
+                             [2, 1], [2, 2], [2, 3],
+                             [3, 1], [3, 2], [3, 3]]])
+        self.assert_serious('[1,2,3][1,2,3]'+chr_cp437(0xF9),
+                            [[[1, 1], [1, 2], [1, 3],
+                             [2, 1], [2, 2], [2, 3],
+                             [3, 1], [3, 2], [3, 3]]])
 
 if __name__ == '__main__':
-    # I/O tests
-    serious_check(chr_cp437(0x09),'a\n','a')
-    serious_check(r',','a\n','"a"')
-    serious_check(r',','abc\n','"abc"')
-    serious_check(r',','12345\n','12345')
-    serious_check(r',','[3, 2, 1]\n','[3,2,1]')
-    serious_check(chr_cp437(0x0C), "abc\n", 'abc')
-
-    # Literals tests
-    serious_check(r'"a','a\n')
-    serious_check(r"'a",'a\n')
-    for i in range(10):
-        serious_check(r'%d'%i,'%d\n'%i)
-    serious_check(r':12345','12345\n')
-    serious_check(r':12345D', '12344\n')
-    serious_check(r':1.25','1.25\n')
-    serious_check(r':1+2j','(1+2j)\n')
-    serious_check(r'[1,2+0j,"fizz",4.0,"buzz"]',"[1, (2+0j), 'fizz', 4.0, 'buzz']\n")
-    serious_check(r'`foo`','foo\n')
-    serious_check(r'"len(set([1,2,2,3]))"{}'.format(chr_cp437(0xF0)), '3\n')
-
-    # Meta stack tests
-    serious_check(r'1 ','1\n1\n')
-    serious_check(r'123(','1\n3\n2\n')
-    serious_check(r'123)','2\n1\n3\n')
-    serious_check(r'123@','2\n3\n1\n')
-    serious_check(r'Q','Q\n')
-    serious_check(r'5W;.D','5\n4\n3\n2\n1\n0\n')
-    serious_check(r'123a','1\n2\n3\n')
-    serious_check(r'123'+chr_cp437(0x7F),'')
-    serious_check(r'123'+chr_cp437(0xB3),'3\n2\n1\n'*2)
-    serious_check(r'123'+chr_cp437(0xC5),'3\n3\n2\n2\n1\n1\n')
-    serious_check(r'12'+chr_cp437(0xC6),'1\n1\n')
-    serious_check(r'123'+chr_cp437(0xFE),'3 2 1\n3\n2\n1\n')
-
-    # Registers tests
-    serious_check(r'1%s2%s%s%s'%(chr_cp437(0xBB),chr_cp437(0xBC),chr_cp437(0xBE),chr_cp437(0xBD)),'1\n2\n')
-    serious_check(r'53%s3%s'%(chr_cp437(0xBF),chr_cp437(0xC0)),'5\n')
-
-    # Math tests
-    serious_check(r'[1][1,2]-','[2]\n')
-    serious_check(r'[1,2,3]M','3\n')
-    serious_check(r'[1,2,3]m','1\n')
-    serious_check(r'[2,3][2,3]*','13\n')
-    serious_check(r'[4][1,2,"3"]q'+chr_cp437(0x8D), '[1, 2]\n')
-    serious_check(r'[4][1,2,"3"]q'+chr_cp437(0x92), "['3']\n")
-    serious_check(r'[4][1,2,"3"]q'+chr_cp437(0xA5), '[[4]]\n')
-    serious_check(r'2'+chr_cp437(0xB9), '[1, 2, 1]\n')
-    serious_check(r':16'+chr_cp437(0xDF), "0123456789ABCDEF\n")
-    serious_check(r'[1,2,3,4]'+chr_cp437(0xE4), "10\n")
-    serious_check(r'[1,2,3,4]'+chr_cp437(0xE3), "24\n")
-    serious_check(r'[1,2,3,4]'+chr_cp437(0xBA), "2.5\n")
-    serious_check(r'[1,2,3,3]'+chr_cp437(0x9A), "3\n")
-    serious_check(r'[2.5, 2.5]'+chr_cp437(0xE4), "5.0\n")
-    serious_check(r'8f', "6\n")
-    serious_check(r'1T'+chr_cp437(0x85), "1.0\n")
-
-    # String tests
-    serious_check(r'[2,3]"{}.{}"f', "2.3\n")
-    serious_check(r'52[2,3,4]T', "[2, 3, 5]\n")
-    serious_check(r'52"234"T', "235\n")
-    serious_check(r'"%s"O'%(chr_cp437(0x57)+chr_cp437(0x58)+chr_cp437(0x59)), "[%s, %s, %s]\n"%(0x57,0x58,0x59))
-    serious_check(r'["%s"]O'%(chr_cp437(0x57)+chr_cp437(0x58)+chr_cp437(0x59)), "[%s, %s, %s]\n"%(0x57,0x58,0x59))
-
-    # Base tests
-    serious_check(r'2:5.5%s'%(chr_cp437(0xAD)), "101.1\n")
-
-    # List tests
-    serious_check(r'2[1,2,3]'+chr_cp437(0xCF), "[[1, 2], [1, 3], [2, 3]]\n")
-    serious_check(r'2[1,2,3]'+chr_cp437(0xD0), "[[1, 2], [1, 3], [2, 1], [2, 3], [3, 1], [3, 2]]\n")
-    serious_check(r'2[1,2,3]'+chr_cp437(0xF9), "[[1, 1], [1, 2], [1, 3], [2, 1], [2, 2], [2, 3], [3, 1], [3, 2], [3, 3]]\n")
-    serious_check(r'[1,2,3][1,2,3]'+chr_cp437(0xF9), "[[1, 1], [1, 2], [1, 3], [2, 1], [2, 2], [2, 3], [3, 1], [3, 2], [3, 3]]\n")
-
-    # More to come...
-
-    assert run_tests()
+    unittest.main(verbosity=3)
