de4f38fb7a81cf7715ff1f7145a75045b7c8f1ce
Ryan Rowe <mega.gamer05@gmail.com>
2016-05-11 01:07:05 -0500

    prepare for setuptools/pypi

---
+++ setup.py
@@ -0,0 +1,47 @@
+"""Seriously - a Python-based golfing language"""
+
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='seriously',
+
+    version='2.0.1',
+
+    description='A Python-based golfing language',
+    long_description=long_description,
+
+    url='https://github.com/Mego/Seriously',
+
+    author='Mego',
+
+    license='MIT',
+
+    classifiers=[
+        'Development Status :: 4 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+
+    keywords='codegolf recreational',
+
+    packages=find_packages(exclude=['docs','htmlcov']),
+
+    # To provide executable scripts, use entry points in preference to the
+    # "scripts" keyword. Entry points provide cross-platform support and allow
+    # pip to create the appropriate form of executable for the target platform.
+    entry_points={
+        'console_scripts': [
+            'seriously=seriously:main',
+        ],
+    },
+)
