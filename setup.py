6f23e050a63c8cc934d8f155f03fe7daa71e78a6
Ryan Rowe <mega.gamer05@gmail.com>
2016-05-12 15:23:26 -0500

    add shell script for use in environments where seriously is not installed

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 setup(
     name='seriously',
 
-    version='2.0.5',
+    version='2.0.6',
 
     description='A Python-based golfing language',
     long_description='Seriously is a Python-based golfing language. See the GitHub page for more details.',
