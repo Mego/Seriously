9d79a3b19965f12de3b86c99680734ea48fca33f
Ryan R <Mego@users.noreply.github.com>
2016-09-20 02:00:51 -0500

    update version number

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 setup(
     name='seriously',
 
-    version='2.0.44',
+    version='2.0.45',
 
     description='A Python-based golfing language',
     long_description='Seriously is a Python-based golfing language. See the GitHub page for more details.',
