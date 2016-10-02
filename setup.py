ead9f646f5276c4f4e417faca4be0ab63a253f88
Ryan R <Mego@users.noreply.github.com>
2016-10-02 07:31:13 -0500

    fix setup script requirements

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 setup(
     name='seriously',
 
-    version='2.0.54',
+    version='2.0.55',
 
     description='A Python-based golfing language',
     long_description='Seriously is a Python-based golfing language. See the GitHub page for more details.',
@@ -25,6 +25,8 @@ setup(
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
     ],
+    
+    install_requires = ['pycryptodome', 'stats'],
     
     packages = ['seriously', 'lib'],
 
