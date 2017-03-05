4dfaaeb9309e953e2cb98714993f672228daf401
Ryan R <Mego@users.noreply.github.com>
2017-03-04 20:37:51 -0600

    better handling of generators (#75)

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ except:
 setup(
     name='seriously',
 
-    version='2.1.0',
+    version='2.1.1',
 
     description='A Python-based golfing language',
     long_description='Seriously is a Python-based golfing language. See the GitHub page for more details.',
