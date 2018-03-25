4648b9b971ad72316bf81350025ae3484690f122
Ryan R <mega.gamer05@gmail.com>
2018-03-25 16:09:23 -0500

    bump

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ except:
 setup(
     name='seriously',
 
-    version='2.1.23',
+    version='2.1.24',
 
     description='A Python-based golfing language',
     long_description='Seriously is a Python-based golfing language. See the GitHub page for more details.',
